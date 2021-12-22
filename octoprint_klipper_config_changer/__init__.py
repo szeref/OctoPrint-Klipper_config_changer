fe08519a74b433476f6827424e42113fe18089ae
szeref <mypubmail333@gmail.com>
2021-12-22 02:56:09 +0100

    fix 2

--- octoprint_klipper_config_changer/__init__.py
+++ octoprint_klipper_config_changer/__init__.py
@@ -6,26 +6,30 @@ import os
 import shutil
 
 class Klipper_config_changerPlugin(octoprint.plugin.TemplatePlugin,
+                       octoprint.plugin.AssetPlugin,
                        octoprint.plugin.SettingsPlugin):
        
 	def get_settings_defaults(self):
     		with open(os.path.expanduser('~/printer.cfg')) as f:
     			tool = f.readline()
     		tool = tool.replace("#", "")
-    		self._logger.info("tool")
     		return dict(tool=tool)
 
 	def on_settings_save(self, data):
 		octoprint.plugin.SettingsPlugin.on_settings_save(self, data)	
 		shutil.copy(os.path.expanduser('~/printer_' + self._settings.get(["tool"]) + '.cfg'), os.path.expanduser('~/printer.cfg'))
 		self._printer.commands("RESTART")
-            	self.logInfo("Reloading Klipper Configuration.")
 
 	def get_template_configs(self):
 		return [
 			dict(type="navbar", custom_bindings=False),
 			dict(type="settings", custom_bindings=False)
 		]
+
+	def get_assets(self):
+		return dict(
+			js=["js/utils.js"]
+		)
 
 __plugin_name__ = "Klipper config changer"
 __plugin_pythoncompat__ = ">=2.7,<4"
